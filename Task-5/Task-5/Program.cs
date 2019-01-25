ffc9dc07713ab163a6b6d2640839add99e11ff13
Eliot Ostling <eostling@cnm.edu>
2019-01-24 19:46:15 -0700

    Graded: Please see TODO comments and resubmit.

--- Task-5/Task-5/Program.cs
+++ Task-5/Task-5/Program.cs
@@ -14,6 +14,7 @@ namespace Task_5
 
             int choice;
             int option= 0 ;
+            //TODO: Local variables need to be lowercase.
             string Question = "Y";
 
             do
@@ -27,8 +28,12 @@ namespace Task_5
             while(option >=2);
 
             {
+                //TODO: Why are you halting the program? This needs to execute some type of logic. 
+                // Example store it to a variable and display it. 
                 Console.ReadLine();
             }
+
+            
 
             ////process
             do
@@ -37,15 +42,17 @@ namespace Task_5
 
                 {
                     Console.WriteLine("Enter number of cups :");
+                    //TODO: Cups is a double not an int
                     cups = int.Parse(Console.ReadLine());
 
                     quarts = cups * 4;
                     Console.WriteLine("The conversion of cups to quarts is:" + quarts);
                 }
-
+                //TODO: This is incorrect. The user only has 2 options not three. 
                 else
                 {
                     Console.WriteLine("Enter number of quarts :");
+                    //TODO: YOu still need to handle if a user enters a String
                     //TODO: You need to handle if the user passes in a String. When I enter J it breaks the program
                     quarts = int.Parse(Console.ReadLine());
                     cups = quarts * 4;
@@ -54,7 +61,7 @@ namespace Task_5
 
                 Console.WriteLine("Do you want to Continue? Y or N");
                 Question = Console.ReadLine();
-
+                //TODO: Loops when I press N it doesnt exit the program. 
             } while (Question != "N");
             Console.ReadLine();
         }
